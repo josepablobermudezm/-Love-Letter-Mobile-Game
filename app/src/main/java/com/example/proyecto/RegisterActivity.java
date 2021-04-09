d39354957d2e47c55a7c2aa6d9aa09fc42108502
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-08 18:46:52 -0600

    Selección de imagen funcional
    Se implementó la funcionalidad para que se pueda seleccionar una imagen desde la vista del registro

--- app/src/main/java/com/example/proyecto/RegisterActivity.java
+++ app/src/main/java/com/example/proyecto/RegisterActivity.java
@@ -3,10 +3,19 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    int SELECT_PICTURE = 200;
+
+    ImageView IVPreviewImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,32 @@ public class RegisterActivity extends AppCompatActivity {
     public void registrarse(View view){
         //me registro
 
+    }
+
+    public void loadPicture(View view){
+        // crea instancia de el intent del tipo de imagen
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pasa esta constante para compararla con el resultado
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // esta función pasa cuando el usuario selecciona algo del imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            // compara el resultado con el SELECT_PICTURE
+            if (requestCode == SELECT_PICTURE) {
+                // obtiene el url de la data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // pone la imagen en la vista
+                    ImageView imageView = (ImageView) findViewById(R.id.IVPreviewImage);
+                    imageView.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 }
