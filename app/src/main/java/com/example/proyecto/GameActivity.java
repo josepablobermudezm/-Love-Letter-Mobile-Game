17b6f96b386a9099cb96bb736b3cd4f46d4b1b26
cjosue13 <44554351+cjosue13@users.noreply.github.com>
2021-06-10 21:05:22 -0600

    XD

--- app/src/main/java/com/example/proyecto/GameActivity.java
+++ app/src/main/java/com/example/proyecto/GameActivity.java
@@ -121,7 +121,7 @@ public class GameActivity extends AppCompatActivity {
                     boolean ok = jsonRespuesta.getBoolean("success");
 
                     if (ok) {
-                        
+
                         Carta carta = cartas.get((int) Math.random()*21 + 1);
                         cartas.remove(carta);
                         Carta carta2 = cartas.get((int) Math.random()*20 + 1);
