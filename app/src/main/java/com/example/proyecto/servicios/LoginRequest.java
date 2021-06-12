82f4a823138c1eef13c43403660a916094d77195
cjosue13 <44554351+cjosue13@users.noreply.github.com>
2021-06-12 11:22:15 -0600

    Arreglo de las packages del proyecto

---
+++ app/src/main/java/com/example/proyecto/servicios/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.example.proyecto.servicios;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    private static final String ruta = "http://winadate.atwebpages.com/login.php";
+    private Map<String, String> parametros;
+    public LoginRequest(String u_alias, String u_password, Response.Listener<String> listener){
+        super(Method.POST, ruta, listener, null);
+        parametros = new HashMap<>();
+        parametros.put("u_alias", u_alias+"");
+        parametros.put("u_password", u_password+"");
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return parametros;
+    }
+
+
+}
