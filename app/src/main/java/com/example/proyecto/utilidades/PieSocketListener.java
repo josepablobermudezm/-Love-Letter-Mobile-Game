08bb21e8cc26ca9508a46731d21f4c31fbda0e81
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-06-27 00:54:29 -0600

    Funcionalidad inicial de principe #5

--- app/src/main/java/com/example/proyecto/utilidades/PieSocketListener.java
+++ app/src/main/java/com/example/proyecto/utilidades/PieSocketListener.java
@@ -265,6 +265,12 @@ public class PieSocketListener extends WebSocketListener {
                         usuario.getMazo().set((usuario.getMazo().get(0) != null) ? 0 : 1, null);
                         usuario.getMazo().set(0, cartaJug2);
                         quitarCartaPrincipe(cartaJug);
+                        if(cartaJug.getNombre().equals("princesa")){
+                            ArrayList<Usuario> array2 = new ArrayList<>();
+                            array2 = (ArrayList<Usuario>) WaitingRoomActivity.usuarios.stream().filter(x -> x.getU_id() == Integer.valueOf(arrSplit_2[1])).collect(Collectors.toList());
+                            princesaJugada(this.context, array2);
+                            WaitingRoomActivity.usuarios.remove(array2.get(0));
+                        }
                     }
                     break;
                 default:
