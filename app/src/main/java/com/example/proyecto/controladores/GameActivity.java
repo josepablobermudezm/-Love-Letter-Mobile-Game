024ba4a75347e3c957bee7d2c23e9e2e4790e97e
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-06-29 17:57:08 -0600

    Lógica de guardia #4

--- app/src/main/java/com/example/proyecto/controladores/GameActivity.java
+++ app/src/main/java/com/example/proyecto/controladores/GameActivity.java
@@ -483,8 +483,16 @@ public class GameActivity extends AppCompatActivity {
                             }
                             else if(modoGuardia){
                                 System.out.println("entrando a metodo de guardia");
-                                onCreateDialog();
+                                AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+                                builder.setTitle("Selecciona una carta")
+                                        .setItems(R.array.cartas_array, new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                System.out.println("carta seleccionada: ");
+                                                System.out.println(which);
+                                            }
+                                        });
 
+                                builder.create();
                                 modoGuardia = false;
                             }
                             listener.enviarMensaje(ws, "cambioTurno");
