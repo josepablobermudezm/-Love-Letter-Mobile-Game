a7e7cdb43c06b0258302be41d704a289b05d827d
JordiRodriguezC <ycorrales120@gmail.com>
2021-07-02 01:42:30 -0600

    nivel

--- app/src/main/java/com/example/proyecto/controladores/GameActivity.java
+++ app/src/main/java/com/example/proyecto/controladores/GameActivity.java
@@ -285,7 +285,7 @@ public class GameActivity extends AppCompatActivity {
                 if(editusuario.isGanado()){
                     editusuario.setU_cantidadPartidasGanadas(editusuario.getU_cantidadPartidasGanadas()+1);
                 }
-                //editusuario.setU_experiencia(editusuario.getU_cantidadPartidasJugadas() * 20);
+                editusuario.setU_experiencia(editusuario.getU_cantidadPartidasJugadas() * 20);
 
                 editusuario.setU_nivel(editusuario.getU_experiencia()/100);
 
